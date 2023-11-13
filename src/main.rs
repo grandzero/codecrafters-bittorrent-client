aed8c6925c3e0e6539b20bb63a7ee59ecde9c63d
Bayram Utku Uzunlar <38724739+grandzero@users.noreply.github.com>
2023-11-13 20:13:48 +0300

    only string newline

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use serde_bencode::de;
 // use serde_bencode::ser::{to_bytes, to_string, Serializer};
+// use serde_bencode::ser::to_string;
 use serde_bencode::value::Value;
+// use serde_json::{to_string as to_string_json, to_string_pretty};
 use std::env;
 
 #[allow(dead_code)]
@@ -40,7 +42,7 @@ fn print_list(decoded_value: &Value, is_nested: bool) {
     } else if let Value::Int(i) = decoded_value {
         print!("{}", i);
     } else if let Value::Bytes(b) = decoded_value {
-        return print!("\"{}\"", String::from_utf8(b.to_vec()).unwrap());
+        return println!("\"{}\"", String::from_utf8(b.to_vec()).unwrap());
         // print!("\n");
     } else {
         print!("unknown for this (dictionary or list)");
@@ -62,6 +64,9 @@ fn main() {
         let encoded_value = &args[2];
 
         let decoded_value = decode_bencoded_value(encoded_value);
+        // println!("{}", to_string_pretty(&decoded_value).unwrap());
+        // println!("{}", to_string(&decoded_value).unwrap());
+        // println!("{}", to_string_json(&decoded_value).unwrap());
         print_list(&decoded_value, false);
         // println!("{:?}", decoded_value);
     } else {
