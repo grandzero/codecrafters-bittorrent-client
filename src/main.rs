414f43583f8a469655194ed5cd444bafc70f6df2
Bayram Utku Uzunlar <38724739+grandzero@users.noreply.github.com>
2023-11-13 20:04:52 +0300

    nested print

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,44 @@ fn decode_bencoded_value(encoded_value: &str) -> Value {
 }
 
 // This is the entry point of your program
-
+fn print_list(decoded_value: &Value, is_nested: bool) {
+    if let Value::List(items) = decoded_value {
+        print!("[");
+        for (pos, item) in items.iter().enumerate() {
+            match item {
+                Value::Int(i) => {
+                    print!("{}{}", i, if pos == items.len() - 1 { "" } else { "," })
+                }
+                Value::Bytes(b) => {
+                    print!(
+                        "\"{}\"{}",
+                        String::from_utf8(b.to_vec()).unwrap(),
+                        if pos == items.len() - 1 { "" } else { "," }
+                    )
+                }
+                Value::List(_) => {
+                    print_list(item, true);
+                }
+                _ => (),
+            };
+        }
+        if items.len() == 0 {
+            return print!("]");
+        } else {
+            print!("]");
+        }
+    } else if let Value::Int(i) = decoded_value {
+        print!("{}", i);
+    } else if let Value::Bytes(b) = decoded_value {
+        return print!("\"{}\"", String::from_utf8(b.to_vec()).unwrap());
+        // print!("\n");
+    } else {
+        print!("unknown for this (dictionary or list)");
+    }
+    if !is_nested {
+        print!("\n")
+    }
+}
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -25,38 +62,7 @@ fn main() {
         let encoded_value = &args[2];
 
         let decoded_value = decode_bencoded_value(encoded_value);
-
-        if let Value::List(items) = decoded_value {
-            print!("[");
-            for (pos, item) in items.iter().enumerate() {
-                match item {
-                    Value::Int(i) => {
-                        print!("{}{}", i, if pos == items.len() - 1 { "" } else { "," })
-                    }
-                    Value::Bytes(b) => {
-                        print!(
-                            "\"{}\"{}",
-                            String::from_utf8(b.to_vec()).unwrap(),
-                            if pos == items.len() - 1 { "" } else { "," }
-                        )
-                    }
-                    _ => (),
-                };
-            }
-            if items.len() == 0 {
-                println!("]");
-            } else {
-                print!("]\n");
-            }
-        } else if let Value::Int(i) = decoded_value {
-            println!("{}", i);
-        } else if let Value::Bytes(b) = decoded_value {
-            println!("\"{}\"", String::from_utf8(b.to_vec()).unwrap());
-            // print!("\n");
-        } else {
-            print!("unknown for this (dictionary or list)");
-        }
-
+        print_list(&decoded_value, false);
         // println!("{:?}", decoded_value);
     } else {
         println!("unknown command: {}", args[1])
