be43c64f9fb1848e11dbd776fa2e7ac43ed43a7d
Bayram Utku Uzunlar <38724739+grandzero@users.noreply.github.com>
2023-11-15 11:01:40 +0300

    decode bencoded dictionary

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use serde_bencode;
 use serde_bencode::de;
+
 // use serde_bencode::ser::{to_bytes, to_string, Serializer};
 // use serde_bencode::ser::to_string;
 use serde_bencode::value::Value;
@@ -10,6 +12,41 @@ fn decode_bencoded_value(encoded_value: &str) -> Value {
     de::from_str(encoded_value).unwrap()
 
     // Convert BencodedList into serde_json::Value
+}
+
+fn print_dictionary(decoded_value: &Value, is_nested: bool) {
+    if let Value::Dict(items) = decoded_value {
+        print!("{{");
+        for (pos, (key, value)) in items.iter().enumerate() {
+            match value {
+                Value::Int(i) => {
+                    print!("\"{}\":{}", String::from_utf8(key.to_vec()).unwrap(), i);
+                }
+                Value::Bytes(b) => {
+                    print!(
+                        "\"{}\":\"{}\"",
+                        String::from_utf8(key.to_vec()).unwrap(),
+                        String::from_utf8(b.to_vec()).unwrap()
+                    );
+                }
+                Value::List(_) => {
+                    print!("\"{}\":", String::from_utf8(key.to_vec()).unwrap(),);
+                    print_list(value, true);
+                }
+                Value::Dict(_) => {
+                    print!("\"{}\":", String::from_utf8(key.to_vec()).unwrap(),);
+                    print_dictionary(value, true);
+                }
+            };
+            if pos != items.len() - 1 {
+                print!(",");
+            }
+        }
+        print!("}}");
+    }
+    if !is_nested {
+        print!("\n")
+    }
 }
 
 // This is the entry point of your program
@@ -34,26 +71,45 @@ fn print_list(decoded_value: &Value, is_nested: bool) {
                         print!(",");
                     }
                 }
-                _ => (),
+                Value::Dict(_) => {
+                    print_dictionary(item, true);
+                    if pos != items.len() - 1 {
+                        print!(",");
+                    }
+                }
             };
         }
-        if items.len() == 0 {
-            return println!("]");
-        } else {
-            print!("]");
-        }
-    } else if let Value::Int(i) = decoded_value {
-        print!("{}", i);
-    } else if let Value::Bytes(b) = decoded_value {
-        return println!("\"{}\"", String::from_utf8(b.to_vec()).unwrap());
-        // print!("\n");
+        print!("]")
+        // if items.len() == 0 {
+        //     return println!("]");
+        // } else {
+        //     print!("]");
+        // }
     } else {
-        print!("unknown for this (dictionary or list)");
+        print!("unknown type");
     }
     if !is_nested {
         print!("\n")
     }
 }
+
+fn print_decoded_value(decoded_value: &Value) {
+    match decoded_value {
+        Value::Int(i) => {
+            print!("{}", i)
+        }
+        Value::Bytes(b) => {
+            print!("{}", String::from_utf8(b.to_vec()).unwrap())
+        }
+        Value::List(_) => {
+            print_list(decoded_value, false);
+        }
+        Value::Dict(_) => {
+            print_dictionary(decoded_value, false);
+        }
+    };
+}
+
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -67,11 +123,8 @@ fn main() {
         let encoded_value = &args[2];
 
         let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", to_string_pretty(&decoded_value).unwrap());
-        // println!("{}", to_string(&decoded_value).unwrap());
-        // println!("{}", to_string_json(&decoded_value).unwrap());
-        print_list(&decoded_value, false);
-        // println!("{:?}", decoded_value);
+
+        print_decoded_value(&decoded_value);
     } else {
         println!("unknown command: {}", args[1])
     }
